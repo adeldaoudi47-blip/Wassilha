079c3faacfc2c4ed64a013ad89400ef0d707592a
Adel DAOUDI <adeldaoudi47@gmail.com>
2026-08-24 11:48:50 +0100

    Prepare Wassilha for production deployment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'dz.wassilha.app',
   appName: 'Wassilha',
-  server: {
-    url: 'http://172.16.0.2:3000',
-    cleartext: true
-  }
+  webDir: 'out'
 };
 
 export default config;
